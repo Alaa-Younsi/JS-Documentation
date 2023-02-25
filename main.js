4073408df5206381e23daabef5bcf14c7ab88934
Philip Marlowe <103404455+Alaa-Younsi@users.noreply.github.com>
2023-02-25 19:48:04 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,138 @@
+
+
+$(document).ready(function(){
+
+  $("#Projects").click(function(){
+      $(".fade").fadeOut(500);
+
+    });
+
+});
+$(document).ready(function(){
+
+  $("#Info").click(function(){
+      $(".fade").fadeOut(500,function(){
+});
+
+    });
+
+});
+
+$(document).ready(function(){
+
+  $("#Contact").click(function(){
+      $("#Home").fadeIn(500);
+
+    });
+});
+
+$(document).ready(function(){
+
+  $("#Info").click(function(){
+      $(".info-text").fadeIn(500,function(){ });
+    });
+});
+
+$(document).ready(function(){
+
+  $("#Info").click(function(){
+      $("#Home").fadeIn(500);
+    });
+});
+
+$(document).ready(function(){
+
+  $("#Home").click(function(){
+      $(".fade").fadeIn(500);
+    });
+});
+
+$(document).ready(function(){
+
+  $("#Home").click(function(){
+      $("#Home").fadeOut(500);
+    });
+});
+
+$(document).ready(function(){
+
+  $("#Home").click(function(){
+      $(".info-text").fadeOut(500,function(){ });
+    });
+});
+
+$(document).ready(function(){
+
+  $("#Projects").click(function(){
+      $("#Home").fadeIn(500);
+    });
+});
+
+$(document).ready(function(){
+
+  $("#Blog").click(function(){
+      $("#Home").fadeIn(500);
+    });
+});
+
+$(document).ready(function(){
+
+  $("#Blog").click(function(){
+      $(".fade").fadeOut(500,function(){
+});
+
+    });
+
+});
+
+$(document).ready(function(){
+
+  $("#Contact").click(function(){
+      $(".fade").fadeOut(500,function(){
+});
+
+    });
+
+});
+
+$(document).ready(function(){
+
+  $("#Projects").click(function(){
+      $(".project-text").fadeIn(500);
+    });
+});
+
+$(document).ready(function(){
+
+  $("#Home").click(function(){
+      $(".project-text").fadeOut(500,function(){ });
+    });
+});
+
+$(document).ready(function(){
+
+  $("#Blog").click(function(){
+      $(".blog-text").fadeIn(500);
+    });
+});
+
+$(document).ready(function(){
+
+  $("#Home").click(function(){
+      $(".blog-text").fadeOut(500,function(){ });
+    });
+});
+
+$(document).ready(function(){
+
+  $("#Contact").click(function(){
+      $(".contact-text").fadeIn(500);
+    });
+});
+
+$(document).ready(function(){
+
+  $("#Home").click(function(){
+      $(".contact-text").fadeOut(500,function(){ });
+    });
+});
